f9ea35f1ff81423634ee7ad1c4300dde1f4e49da
Anna <ania.jasica@gmail.com>
2019-12-09 16:17:25 +0100

    display tasks from array

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,21 @@ const tasks = require("./routes/routes");
 
 const app = express();
 
+const examplesTasks = [
+  {
+    taskDescription: "Lorem ipsum dolor, sit amet consecteturadipisicing elit."
+  },
+  {
+    taskDescription: "Lorem ipsum dolor, sit amet consecteturadipisicing elit."
+  },
+  {
+    taskDescription: "Lorem ipsum dolor, sit amet consecteturadipisicing elit."
+  }
+];
+
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
+
 mongoose
   .connect("mongodb://localhost/tasks", {
     useNewUrlParser: true,
@@ -13,6 +28,9 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("Could not connect to MongoDB...", err));
 
+app.use(express.static("../"));
+app.set("views", __dirname + "/views");
+app.engine("html", require("ejs").renderFile);
 app.use(express.json());
 app.use("/api/tasks", tasks);
 
@@ -29,6 +47,13 @@ const taskSchema = new mongoose.Schema({
   },
   author: String
 });
+
+app.get("/tasks", (req, res) => {
+  res.render("tasks", {
+    tasks: examplesTasks
+  });
+});
+
 const Task = mongoose.model("Task", taskSchema);
 
 const port = process.env.PORT || 3000;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,52 +1,52 @@
-const express = require('express');
+const express = require("express");
 
-const _ = require('lodash');
-const Joi = require('joi');
+const _ = require("lodash");
+const Joi = require("joi");
 const router = express.Router();
+const path = require("path");
 
-router.post('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../../login.html"));
+});
 
-    if (!req.body.text || req.body.text < 5) {
-        //400 Bad Request
-        res.status(400).send('the text is required and it should take at least 5 characters')
-        return;
+router.post("/", async (req, res) => {
+  if (!req.body.text || req.body.text < 5) {
+    //400 Bad Request
+    res
+      .status(400)
+      .send("the text is required and it should take at least 5 characters");
+    return;
+  }
+  let task = new Task(_.pick(req.body, ["text", "status", "author"]));
+  // let customer = new Customer({
+  //     name: req.body.name,
+  //     isGold: req.body.isGold,
+  //     phone: req.body.phone
+  // });
+  try {
+    task = await task.save();
+
+    res.send(task);
+  } catch (err) {
+    res.status(500).send();
+    console.log(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await Task.deleteOne({
+      _id: req.params.id
+    }); //method takes filter or query object
+
+    if (result.deletedCount === 0) {
+      res.status(404).send(result);
+    } else {
+      res.send(result);
     }
-    let task = new Task(_.pick(req.body, ['text', 'status', 'author']))
-    // let customer = new Customer({
-    //     name: req.body.name,
-    //     isGold: req.body.isGold,
-    //     phone: req.body.phone
-    // });
-    try {
-        task = await task.save();
-
-        res.send(task);
-    }
-    catch (err) {
-        res.status(500).send()
-        console.log(err)
-
-    }
-
-})
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const result = await Task.deleteOne({ _id: req.params.id }) //method takes filter or query object
-
-        if (result.deletedCount === 0) {
-            res.status(404).send(result);
-        } else {
-
-            res.send(result);
-        }
-
-    }
-    catch (err) {
-        res.status(500).send();
-        console.log(err);
-    }
-
-}
-)
+  } catch (err) {
+    res.status(500).send();
+    console.log(err);
+  }
+});
 module.exports = router;
