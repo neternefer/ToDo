1f995de20476aef78ca967caf307517faa69bb57
Anna <ania.jasica@gmail.com>
2019-12-12 13:23:45 +0100

    moved routes from index to routes

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const _ = require("lodash");
 const mongoose = require("mongoose");
-const tasks = require("./routes/routes");
+const routes = require("./routes/tasks.js");
 var bodyParser = require("body-parser");
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.use("/public", express.static("public"));
+app.set("views", __dirname + "/views");
+app.use("/", express.static("../"));
+app.engine("html", require("ejs").renderFile);
+app.use(express.json());
+app.use("/tasks", routes);
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 mongoose
   .connect(
-    "mongodb+srv://user1:3saKGtSfvK2sEaj3@cluster0-kjmeo.mongodb.net/test?retryWrites=true&w=majority",
-    {
+    "mongodb+srv://user1:3saKGtSfvK2sEaj3@cluster0-kjmeo.mongodb.net/test?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true
     }
@@ -20,54 +28,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("Could not connect to MongoDB...", err));
 
-app.use(express.static("../"));
-app.set("views", __dirname + "/views");
-app.engine("html", require("ejs").renderFile);
-app.use(express.json());
-app.use("/api/tasks", tasks);
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-const taskSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: ["to_do", "in_progress", "done"]
-  },
-  author: String
-});
-const Task = mongoose.model("Task", taskSchema);
-
-app.get("/tasks", async (req, res) => {
-  const tasks = await Task.find();
-  console.log(tasks);
-  res.render("tasks", {
-    tasks: tasks
-  });
-});
-
-app.put("/updateTask", async (req, res) => {
-  let result;
-  try {
-    result = await Task.updateOne(
-      { _id: req.body.id },
-      { text: req.body.text, status: req.body.status }
-    );
-    res.send("Task updated");
-  } catch (error) {
-    res.send("Update was unsuccessful");
-  }
-});
-
 const port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`listening on port ${port}...`);
 });
---
+++ backend/routes/tasks.js
@@ -0,0 +1,74 @@
+const express = require("express");
+
+const _ = require("lodash");
+const Joi = require("joi");
+const router = express.Router();
+const path = require("path");
+const Task = require("../model/task.js")
+
+// router.get("/", async (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../login.html"));
+// });
+
+// router.post("/", async (req, res) => {
+//   if (!req.body.text || req.body.text < 5) {
+//     //400 Bad Request
+//     res
+//       .status(400)
+//       .send("the text is required and it should take at least 5 characters");
+//     return;
+//   }
+//   let task = new Task(_.pick(req.body, ["text", "status", "author"]));
+//   try {
+//     task = await task.save();
+
+//     res.send(task);
+//   } catch (err) {
+//     res.status(500).send();
+//     console.log(err);
+//   }
+// });
+
+router.get("/", async (req, res) => {
+  const tasks = await Task.find();
+  console.log(tasks);
+  res.render("tasks", {
+    tasks: tasks
+  });
+});
+
+// body should include: id, text, status
+router.put("/", async (req, res) => {
+  let result;
+  try {
+    console.log(req.body);
+    result = await Task.updateOne({
+      _id: req.body.id
+    }, {
+      text: req.body.text,
+      status: req.body.status
+    });
+    console.log(result);
+    res.send("Task updated");
+  } catch (error) {
+    res.send("Update was unsuccessful");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await Task.deleteOne({
+      _id: req.params.id
+    }); //method takes filter or query object
+
+    if (result.deletedCount === 0) {
+      res.status(404).send(result);
+    } else {
+      res.send(result);
+    }
+  } catch (err) {
+    res.status(500).send();
+    console.log(err);
+  }
+});
+module.exports = router;
